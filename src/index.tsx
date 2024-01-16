ad80d5d1194fc8ba6886ee7a808fb0a3f5c95169
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-01-16 23:13:23 +0900

    feat: reactquery setting

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,17 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routers'
 import { RecoilRoot } from 'recoil'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
-        <RecoilRoot>
-            <RouterProvider router={router} />
-        </RecoilRoot>
+        <QueryClientProvider client={queryClient}>
+            <RecoilRoot>
+                <RouterProvider router={router} />
+            </RecoilRoot>
+        </QueryClientProvider>
     </React.StrictMode>,
 )
