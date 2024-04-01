bdebf1b5f09f6e75ce64ffc5f54485a2253106df
[SW5] 백민혁 <minhyeokbaek100@gmail.com>
2024-04-01 18:20:24 +0900

    feat: 배포준비중

---
+++ src/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import firebase from 'firebase/compat/app'
+import { getFirestore } from 'firebase/firestore/lite'
+import 'firebase/compat/storage'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+}
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig)
+const db = getFirestore(app)
+const storage = firebase.storage()
+
+export { db, storage }
